fb275045a7b1476fa0d71e99d980952a9a3d25fa
male-brother-likes-meat <1304055639@qq.com>
2022-10-17 10:08:22 +0800

    添加项目文件。

---
+++ test-pro/Pages/User.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace test_pro.Pages
+{
+    public class UserModel:PageModel
+    {
+        private readonly ILogger<UserModel> _logger;
+
+        public UserModel(ILogger<UserModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
